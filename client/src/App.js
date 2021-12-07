6ef185d89d597aee2e3e4261ddb8fc1e6d933491
김명식 <kimmyeongsik@gimmyeongsig-ui-MacBookPro.local>
2021-12-07 22:31:24 +0900

    12.07_sik_last1

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import {
 import LandingPage from './components/views/LandingPage/LandingPage'
 import LoginPage from './components/views/LoginPage/LoginPage'
 import RegisterPage from './components/views/RegisterPage/RegisterPage'
+import Auth from './hoc/auth'
 
 function App() {
   return (
@@ -24,9 +25,14 @@ function App() {
           of them to render at a time
         */}
         <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
+          {/* Auth()는 hoc폴더안에 있는 auth.js 파일이다.*/}
+          {/* Auth()안에 있는 null은 auth.js안에 있는 함수의 option 매게변수이다. */}
+          {/* LandingPage는 아무나 들어갈 수 있기 때문에 option = null 이다. */}
+          <Route path="/" element={Auth(<LandingPage />, null)} />
+          {/* LoginPage는 로그인한 유저는 출입이 불가능해야하기 때문에 option = false 이다. */}
+          <Route path="/login" element={Auth(<LoginPage />, false)} />
+          {/* RegisterPage(회원가입)는 로그인한 유저는 출입이 불가능해야하기 때문에 option = false 이다. */}
+          <Route path="/register" element={Auth(<RegisterPage />, false)} />
         </Routes>
       </div>
     </Router>
--- client/src/hoc/auth.js
+++ client/src/hoc/auth.js
@@ -6,6 +6,11 @@ import { auth } from "../_actions/user_action";
 
 export default function (SpecificComponent, option, adminRoute = null){
 
+    //**** option ****/
+    //null  =>  아무나 출입이 가능한 페이지
+    //true  =>  로그인한 유저만 출입이 가능한 페이지
+    //false  =>  로그인한 유저는 출입 불가능한 페이지
+
     function AuthenticationCheck(){
 
         // redux를 사용하기 위해 useDispatch() 사용
