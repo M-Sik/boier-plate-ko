0810f2cfc4ea1a755068731dae2bd31f7ba2959c
김명식 <kimmyeongsik@gimmyeongsig-ui-MacBookPro.local>
2021-12-09 09:52:13 +0900

    12.09_sik_최종

--- client/src/App.js
+++ client/src/App.js
@@ -28,11 +28,12 @@ function App() {
           {/* Auth()는 hoc폴더안에 있는 auth.js 파일이다.*/}
           {/* Auth()안에 있는 null은 auth.js안에 있는 함수의 option 매게변수이다. */}
           {/* LandingPage는 아무나 들어갈 수 있기 때문에 option = null 이다. */}
-          <Route path="/" element={Auth(<LandingPage />, null)} />
+          <Route path="/" element={Auth(LandingPage, null)} />
+
           {/* LoginPage는 로그인한 유저는 출입이 불가능해야하기 때문에 option = false 이다. */}
-          <Route path="/login" element={Auth(<LoginPage />, false)} />
+          <Route path="/login" element={Auth(LoginPage, false)} />
           {/* RegisterPage(회원가입)는 로그인한 유저는 출입이 불가능해야하기 때문에 option = false 이다. */}
-          <Route path="/register" element={Auth(<RegisterPage />, false)} />
+          <Route path="/register" element={Auth(RegisterPage, false)} />
         </Routes>
       </div>
     </Router>
